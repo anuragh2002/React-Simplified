546a573da7ce651e29930ec800e381afa7638799
Anuragh Barua <anuraghbarua@gmail.com>
2026-05-10 23:48:08 +0530

    Created a counter app to learn useState hook

--- Basic Stateful Components/vite-app/src/App.jsx
+++ Basic Stateful Components/vite-app/src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hi</div>;
+  const [counter, setCounter] = useState(0);
+  console.log("re render");
+
+  function handleDecrement() {
+    if (counter === 0) return;
+    setCounter(counter - 1);
+  }
+
+  function handleIncrement() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleDecrement}>-</button>
+      {counter}
+      <button onClick={handleIncrement}>+</button>
+    </div>
+  );
 }
 
 export default App;
