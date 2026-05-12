f739e1c9b1b6102d9f7197431f8de146574ada8c
Anuragh Barua <anuraghbarua@gmail.com>
2026-05-12 22:55:11 +0530

    Learnt how state re renders the whole component

--- Basic Stateful Components/vite-app/src/App.jsx
+++ Basic Stateful Components/vite-app/src/App.jsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  console.log("re render");
+  const [name, setName] = useState(slowGetter);
 
-  function handleDecrement() {
-    if (counter === 0) return;
-    setCounter(counter - 1);
+  function slowGetter() {
+    console.log("State setter");
+    return "Kyle";
   }
 
-  function handleIncrement() {
-    setCounter(counter + 1);
+  function handleClick() {
+    setName("Anuragh");
+    console.log("Re - render");
   }
+
+  // const [counter, setCounter] = useState(0);
+  // console.log("re render");
+
+  // function handleDecrement() {
+  //   if (counter === 0) return;
+  //   setCounter(counter - 1);
+  // }
+
+  // function handleIncrement() {
+  //   setCounter(counter + 1);
+  // }
 
   return (
     <div>
-      <button onClick={handleDecrement}>-</button>
-      {counter}
-      <button onClick={handleIncrement}>+</button>
+      {/* <button onClick={handleDecrement}>-</button>
+      {/* Calling handleDecrement function on click of - button but without parenthesis  
+      because we don't want onCick to be equal to what will be returned by handleDecrement function 
+      but we want it to be equal to the function itself so that it can be called when the button is clicked */}
+      {/* {counter} */}
+      {/* <button onClick={handleIncrement}>+</button> } */}
+      <h1 onClick={handleClick}>Hi {name}</h1>
     </div>
   );
 }
